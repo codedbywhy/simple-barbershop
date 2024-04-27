a1fb3aa86c3a39eb70784389df9c050da3277f92
Wahyu Pratama <w.prtama20@gmail.com>
2024-04-28 00:07:36 +0700

    test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Function to handle product click event
         function handleProductClick(productName) {
             const message = `Apakah ${productName} tersedia?`;
-            const waLink = `https://wa.me/6281364947152?text=${encodeURIComponent(message)}`;
+            const waLink = `https://wa.me/6282286141946?text=${encodeURIComponent(message)}`;
             window.open(waLink, "_blank");
         }
