8513a10a857098cb2a617a77bc957bda245f7fe8
Wahyu Pratama <w.prtama20@gmail.com>
2024-05-03 22:06:11 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 // Function to handle product click event
-        function handleProductClick(productName) {
-            const message = `Apakah ${productName} tersedia?`;
-            const waLink = `https://wa.me/6282286141946?text=${encodeURIComponent(message)}`;
-            window.open(waLink, "_blank");
-        }
+function handleProductClick(productName) {
+    const message = `Apakah ${productName} tersedia?`;
+    const waLink = `https://wa.me/6282286141946?text=${encodeURIComponent(message)}`;
+    window.open(waLink, "_blank");
+}
+
+//typing text
+const text = "DIM'S BARBERSHOP";
+let index = 0;
+const typingTitle = document.getElementById('typing-title');
+
+function type() {
+    if (index < text.length) {
+        typingTitle.textContent += text.charAt(index);
+        index++;
+        setTimeout(type, 100);
+    } else {
+        
+        typingTitle.textContent = '';
+        index = 0;
+        setTimeout(type, 1000);
+    }
+}
+
+type();
